3fe43045d7949d5866821b45cc34e8ec9e855ef8
qudsiramiz <qudsiramiz@gmail.com>
2023-11-08 16:11:09 -0500

    minor changes

--- codes/jet_reversal_check.py
+++ codes/jet_reversal_check.py
@@ -12,18 +12,18 @@ import jet_reversal_check_function as jrcf
 importlib.reload(jrcf)
 
 # Read the data from csv files
-df_crossings = pd.read_csv("../data/mms_magnetopause_crossings.csv", index_col=False)
+df_crossings = pd.read_csv("../data/brst_intervals.csv", index_col=False)
 
 # Set the index to the date column
-df_crossings.set_index("DateStart", inplace=True)
+df_crossings.set_index("start_time", inplace=True)
 
-date_obs = "20230721"
+date_obs = "20230901"
 
 
 def check_jet_reversal(crossing_time):
     # Convert the crossing time to a datetime object
     crossing_time = datetime.datetime.strptime(crossing_time.split("+")[0],
-                                               "%Y-%m-%d %H:%M:%S.%f")
+                                               "%Y-%m-%d %H:%M:%S")
     # Set the timezone to UTC
     crossing_time = crossing_time.replace(tzinfo=pytz.utc)
     print(f"crossing_time = {crossing_time}")
@@ -44,33 +44,33 @@ def check_jet_reversal(crossing_time):
               "verbose": True
               }
 
-    # inputs["data_rate"] = "brst"
-    # _ = jrcf.jet_reversal_check(**inputs)
-    try:
-        try:
-            inputs["data_rate"] = "brst"
-            _ = jrcf.jet_reversal_check(**inputs)
-        except Exception:
-            # inputs["data_rate"] = "brst"
-            # _ = jrcf.jet_reversal_check(**inputs)
-            pass
-    except Exception as e:
-        print(f"\033[91;31m\n{e} for date {crossing_time}\n\033[0m")
-        # Save the crossing time to a file
-        # Check if the file exists
-        if not os.path.isfile(inputs["error_file_log_name"]):
-            # If it doesn"t exist, create it
-            with open(inputs["error_file_log_name"], "w") as f:
-                f.write("DateStart,Error\n")
-                f.write(f"{crossing_time},{e}\n")
-        else:
-            # If it exists, append to it
-            df_added_list = pd.read_csv(inputs["error_file_log_name"], sep=",", index_col=False)
-            if not np.any(df_added_list["DateStart"].values == str(crossing_time)):
-                with open(inputs["error_file_log_name"], "a") as f:
-                    f.write(f"{crossing_time},{e}\n")
-                f.close()
-        pass
+    inputs["data_rate"] = "brst"
+    _ = jrcf.jet_reversal_check(**inputs)
+    # try:
+    #     try:
+    #         inputs["data_rate"] = "brst"
+    #         _ = jrcf.jet_reversal_check(**inputs)
+    #     except Exception:
+    #         # inputs["data_rate"] = "brst"
+    #         # _ = jrcf.jet_reversal_check(**inputs)
+    #         pass
+    # except Exception as e:
+    #     print(f"\033[91;31m\n{e} for date {crossing_time}\n\033[0m")
+    #     # Save the crossing time to a file
+    #     # Check if the file exists
+    #     if not os.path.isfile(inputs["error_file_log_name"]):
+    #         # If it doesn"t exist, create it
+    #         with open(inputs["error_file_log_name"], "w") as f:
+    #             f.write("DateStart,Error\n")
+    #             f.write(f"{crossing_time},{e}\n")
+    #     else:
+    #         # If it exists, append to it
+    #         df_added_list = pd.read_csv(inputs["error_file_log_name"], sep=",", index_col=False)
+    #         if not np.any(df_added_list["DateStart"].values == str(crossing_time)):
+    #             with open(inputs["error_file_log_name"], "a") as f:
+    #                 f.write(f"{crossing_time},{e}\n")
+    #             f.close()
+    #     pass
 
     return None
 
@@ -88,6 +88,9 @@ indx_min = int(input("Enter the minimum index number: "))
 indx_max = int(input("Enter the maximum index number: "))
 
 if __name__ == "__main__":
-    with suppress_stdout_stderr():
-        for xx, crossing_time in enumerate(df_crossings.index[indx_min:indx_max], start=indx_min):
+    # with suppress_stdout_stderr():
+    for xx, crossing_time in enumerate(df_crossings.index[indx_min:indx_max], start=indx_min):
+        try:
             check_jet_reversal(crossing_time)
+        except Exception as e:
+            pass
